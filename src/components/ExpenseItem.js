ac0d31032467a28ed430c0abd5b478367f3e9e02
Sady <sadydoucoure@gmail.com>
2022-08-26 16:35:41 -0400

    Expense Iteme finalization

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
-const ExpenseItem = ()=>{
-    return <h2>Expense Item</h2>
-}
+import Card from "./Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.expense.date} />
+      <div className="expense-item__description">
+        <h2>{props.expense.title}</h2>
+        <div className="expense-item__price">{props.expense.amount}</div>
+      </div>
+    </Card>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
---
+++ src/components/Expenses.js
@@ -0,0 +1,15 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem expense={props.expenses[0]} />
+      <ExpenseItem expense={props.expenses[1]} />
+      <ExpenseItem expense={props.expenses[2]} />
+      <ExpenseItem expense={props.expenses[3]} />
+    </Card>
+  );
+}
+
+export default Expenses;
