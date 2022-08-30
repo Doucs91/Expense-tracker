6b59eb7f2afef5f6b957107bd4fa32def013a64a
Sady <sadydoucoure@gmail.com>
2022-08-30 14:35:29 -0400

    Adding filter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,20 @@
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 const Expenses = (props) => {
+  const onFilterHandler = (year) => {
+    console.log(year);
+  };
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => {
-        return <ExpenseItem key={expense.id} expense={expense} />;
-      })}
-    </Card>
+    <div className="expenses">
+      <ExpensesFilter onFilter={onFilterHandler} />
+      <Card className="expenses">
+        {props.expenses.map((expense) => {
+          return <ExpenseItem key={expense.id} expense={expense} />;
+        })}
+      </Card>
+    </div>
   );
 };
 
